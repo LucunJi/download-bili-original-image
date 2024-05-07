58747a1af328132af489e0dea557bd06b86973a9
LucunJi <36262513+LucunJi@users.noreply.github.com>
2024-05-07 04:44:19 -0400

    feat: use context menu instead

--- main.js
+++ main.js
@@ -1,63 +1,80 @@
 // ==UserScript==
-// @name         Download bili original image
+// @name         下载 bilibili 原图
 // @namespace    LucunJi
 // @version      1.0.0
-// @description  Download image as its original format from bilibili
+// @description  以原来的格式右键下载 bilibili 动态和评论区中的图片，而不是 webp 或 avif
 // @author       Lucun_Ji
 // @license      WTFPL
 // @match        https://*.bilibili.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @icon         https://t.bilibili.com/favicon.ico
 // @homepage     https://github.com/LucunJi/download-bili-original-image
 // @supportURL   https://github.com/LucunJi/download-bili-original-image/issues
 // @grant        none
+// @require https://code.jquery.com/jquery-3.7.1.slim.min.js#sha256=9261efb3407e3a9096e4654750d8eff6b3a663422f48845c7fbcc65034c340cf
 // ==/UserScript==
 
-(function() {
-    let styleSheet = document.createElement('style');
-    styleSheet.innerText = `
-        .down_orig_button {
-            transform: translate(10%, -110%);
-            opacity: 0;
-            transition: opacity 0.5s;
-        }
-        .down_orig_button:hover {
-            opacity: 1;
-        }
-        `;
-    document.head.appendChild(styleSheet);
 
-    function downloadImg(imgSrc, filename) {
-        fetch(imgSrc).then(resp => resp.blob()).then(blob => {
-            const a = document.createElement('a');
-            a.href = URL.createObjectURL(blob);
-            a.download = filename;
-            a.click();
-            delete a;
-        });
+(function() {
+    $(`
+<style>
+#download-original-image-menu:hover {
+    background-color: lightskyblue;
+    color: white;
+}
+
+#download-original-image-menu {
+    position: absolute;
+    z-index: 999;
+    user-select: none;
+    font-size: small;
+    padding: 5px 8px;
+    border-radius: 3px;
+    color: black;
+    background-color: whitesmoke;
+}
+</style>`).appendTo('head')
+
+    const menu = $('<div id="download-original-image-menu" style="display: none;">下载原图（Shift+右击打开浏览器菜单）</div>');
+    menu.appendTo('body');
+
+    function closeMenu() {
+        menu.css('display', 'none');
+        menu.off();
     }
 
-    window.setInterval(() => {
-        let previews = document.getElementsByClassName('bili-album__preview__picture');
-        for (const node of previews) {
-            if (!node.classList.contains('has_download_original_button')) {
-                let downBtn = document.createElement('button');
-                downBtn.innerHTML = '<u>⤓</u>';
-                downBtn.classList.add('down_orig_button');
-                node.appendChild(downBtn);
+    document.addEventListener('contextmenu', function(event) {
+        const tgt = event.target;
+        if (tgt.nodeName === 'IMG' && $(tgt).parents('.bili-album, .image-exhibition').length) {
+            event.preventDefault();
 
-                downBtn.addEventListener('click', function (event) {
-                    event.stopPropagation();
+            menu.css({
+                'display': '',
+                'top': event.pageY,
+                'left': event.pageX
+            });
+            const imgSrc = new URL(tgt.src);
+            imgSrc.protocol = 'https:'; // prevents mixed content if protocol is http:
+            imgSrc.pathname = imgSrc.pathname.replace(/@.*\.(avif|webp)$/, '');
+            const imgName = imgSrc.pathname.split('/').pop();
+            menu.on('click', function(eventMenu) {
+                console.log(eventMenu.data);
+                fetch(imgSrc).then(resp => resp.blob()).then(blob => {
+                    const a = document.createElement('a');
+                    a.href = URL.createObjectURL(blob);
+                    a.download = imgName;
+                    a.click();
+                    delete a;
+                });
 
-                    const imgUrl = node.getElementsByTagName('img')[0]?.src;
-                    if (imgUrl === undefined) return;
-                    const imgUrlOrig = imgUrl.replace(/@.*\.(avif|webp)$/, '');
-                    const urlParts = imgUrlOrig.split('/');
-                    const imgName = urlParts[urlParts.length - 1];
-                    downloadImg(imgUrlOrig, imgName);
-                }, { capture: true });
-
-                node.classList.add('has_download_original_button');
-            }
+                closeMenu();
+            });
         }
-    }, 500);
+    });
+
+    document.addEventListener('click', function(event) {
+        console.log(event);
+        if (event.target.id !== 'download-original-image-menu') {
+            closeMenu();
+        }
+    });
 })();
