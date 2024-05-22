670890742a102527f2cc38d3cb33b795a61c61e1
LucunJi <36262513+LucunJi@users.noreply.github.com>
2024-05-22 02:55:09 -0400

    support img hidden in shadowRoot

--- main.js
+++ main.js
@@ -9,12 +9,22 @@
 // @icon         https://t.bilibili.com/favicon.ico
 // @homepage     https://github.com/LucunJi/download-bili-original-image
 // @supportURL   https://github.com/LucunJi/download-bili-original-image/issues
+// @run-at document-start
 // @require https://code.jquery.com/jquery-3.7.1.slim.min.js#sha256=9261efb3407e3a9096e4654750d8eff6b3a663422f48845c7fbcc65034c340cf
 // ==/UserScript==
 
+(function () {
+    // inject into attachShadow function to force them open, requires @run-at document-start
+    (function (original) {
+        Element.prototype.attachShadow = function(options) {
+            const optionsNew = structuredClone(options);
+            optionsNew.mode = 'open';
+            return original.apply(this, [optionsNew]);
+        };
+    })(Element.prototype.attachShadow);
 
-(function() {
-    $(`
+    $(document).ready(function () {
+        $(`
 <style>
 #download-original-image-menu:hover {
     background-color: lightskyblue;
@@ -23,7 +33,7 @@
 
 #download-original-image-menu {
     position: absolute;
-    z-index: 999999;
+    z-index: calc(infinity);
     user-select: none;
     font-size: small;
     padding: 5px 8px;
@@ -33,25 +43,33 @@
 }
 </style>`).appendTo('head')
 
-    const menu = $('<div id="download-original-image-menu" style="display: none;">下载原图（Shift+右击打开浏览器菜单）</div>');
-    menu.appendTo('body');
+        const menu = $('<div id="download-original-image-menu" style="display: none;">下载原图（Shift+右击打开浏览器菜单）</div>');
+        menu.appendTo('body');
 
-    function closeMenu() {
-        menu.css('display', 'none');
-        menu.off();
-    }
+        function closeMenu() {
+            menu.css('display', 'none');
+            menu.off();
+        }
 
-    document.addEventListener('contextmenu', function(event) {
-        const tgt = event.target;
-        if (tgt.nodeName === 'IMG' && $(tgt).parents('.bili-album, .image-exhibition, .bili-gallery').length) {
+        document.addEventListener('contextmenu', function(event) {
+            const tgt = event.target;
+            let imgSrc = undefined;
+            if (tgt.nodeName.toLowerCase() === 'img' && $(tgt).parents('.bili-album, .image-exhibition, .bili-gallery').length) {
+                imgSrc = tgt.src;
+            } else if (tgt.nodeName.toLowerCase().startsWith('preview-image-') && tgt.shadowRoot) {
+                imgSrc = $(tgt.shadowRoot).children().find('.show-image-wrap > .image-content')[0]?.src;
+            }
+
+            if (!imgSrc) return;
+
             event.preventDefault();
-
             menu.css({
                 'display': '',
                 'top': event.pageY,
                 'left': event.pageX
             });
-            const imgSrc = new URL(tgt.src);
+
+            imgSrc = new URL(imgSrc);
             imgSrc.protocol = 'https:'; // prevents mixed content if protocol is http:
             imgSrc.pathname = imgSrc.pathname.replace(/@.*\.(avif|webp)$/, '');
             const imgName = imgSrc.pathname.split('/').pop();
@@ -66,13 +84,12 @@
 
                 closeMenu();
             });
-        }
-    });
+        });
 
-    document.addEventListener('click', function(event) {
-        console.log(event);
-        if (event.target.id !== 'download-original-image-menu') {
-            closeMenu();
-        }
-    }, true); // set capture=true in case that other elements prevent the event
+        document.addEventListener('click', function(event) {
+            if (event.target.id !== 'download-original-image-menu') {
+                closeMenu();
+            }
+        }, true); // set capture=true in case that other elements prevent the event
+    });
 })();
